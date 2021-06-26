9543c1214bbac86445c26d8bc62cb4f4fb8d4b24
Nathanael Gastelum <nathanaelgastelum@gmail.com>
2021-06-25 17:16:33 -0700

    attempted to implement smallest k with duplicates

--- Cracking the Coding Interview/smallest_k.py
+++ Cracking the Coding Interview/smallest_k.py
@@ -1,5 +1,7 @@
 # Question 17.14
 
+import random
+
 # O(n log n), can be done faster with Max Heap or selection rank
 def smallest_k(lst, k):
     lst.sort()
@@ -9,3 +11,57 @@ def smallest_k(lst, k):
 actual = smallest_k([3,4,2,5,1], 3)
 expected = [1,2,3]
 assert actual == expected, f"{actual}"
+
+
+# Selection rank Solution (if elements are unique)
+def smallest_k_selection(lst, k):
+    threshold = rankf(lst, k - 1)
+    smallest = [0 for x in range(k)]
+    count = 0
+
+    for a in lst:
+        if a < threshold:
+            smallest[count] = a
+            count += 1
+
+    return smallest
+
+def rankf(lst, rank, left=None, right=None):
+    if left == None: left = 0
+    if right == None: right = len(lst)-1
+
+    pivot = lst[random.randint(left, right)] 
+    left_end = partition(lst, left, right, pivot)
+    left_size = left_end - left + 1
+    if rank == left_size - 1:
+        return max(lst[left : left_end], default=lst[left])
+    elif rank < left_size:
+        return rankf(lst, rank, left, left_end)
+    else:
+        return rankf(lst, rank-left_size, left_end+1, right)
+
+def partition(lst, left, right, pivot):
+    while left <= right:
+        if lst[left] > pivot:
+            # Left is bigger than pivot. Swap it to the right side where we know it should be
+            lst[left], lst[right] = lst[right], lst[left]
+            right -= 1
+        elif lst[right] <= pivot:
+            lst[left], lst[right] = lst[right], lst[left]
+            left += 1
+        else:
+            left += 1
+            right -= 1
+
+    return left - 1
+
+test_lst = [5,1,3,4,2]
+actual = smallest_k_selection(test_lst, 3)
+expected = [1, 2, 3]
+assert actual.sort() == expected, f"actual: {actual} expected: {expected}"
+
+# Testing with duplicates
+test_lst = [3, 1, 5, 1, 2, 4]
+actual = smallest_k_selection(test_lst, 3)
+expected = [1, 2, 3]
+assert actual.sort() == expected, f"actual: {actual} expected: {expected}"
--- quicksort.py
+++ quicksort.py
@@ -2,24 +2,30 @@
 # Chooses a section of a list and a pivot, moves everything in the section
 # based on its relation to the pivot
 
-def quicksort(lst):
+def quicksort(lst, low, high):
     if len(lst) == 1 or len(lst) == 0: return
 
-    # Select median pivot
-    pivot = len(lst) // 2
+    # Select median pivot and move to end of list
+    midpoint = (high - low) // 2
+    lst[midpoint], lst[high-1] = lst[high-1], lst[midpoint]
+    pivot = lst[-1]
 
-    for i in range(len(lst)):
-        if lst[i] < lst[pivot]:
-            lst.insert(pivot, lst.pop(i))
-        else:
-            lst.insert(pivot+1, lst.pop(i))
+    # i keeps track of how many elements have been moved
+    i = 0
+    for j in range(low, high):
+        if lst[j] < pivot:
+            lst.insert(i, lst.pop(j))
+            i += 1
+
+    # Move pivot back to correct location, only needed if strict inequality on the left
+    lst.insert(i, lst.pop(high-1))
 
     # Recrusion on left and right partitions
-    quicksort(lst[:pivot])
-    quicksort(lst[pivot:])
+    quicksort(lst, low, i)
+    quicksort(lst, i, high)
 
-test_lst = [3,1,5,4,2]
-quicksort(test_lst)
+test_lst = [5,1,3,4,2]
+quicksort(test_lst, 0, len(test_lst))
 actual = test_lst
 expected = [1,2,3,4,5]
 assert actual == expected, f"actual: {actual} expected: {expected}"
