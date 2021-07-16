8a63a7e6e50bd60ef5eaab69a52123d9deddb461
Nathanael Gastelum <nathanaelgastelum@gmail.com>
2021-07-16 14:32:17 -0700

    completed decoder for 1.6

--- Cracking the Coding Interview/string_compression.py
+++ Cracking the Coding Interview/string_compression.py
@@ -5,25 +5,70 @@
 # output: compressed string
 
 def compress_string(string):
-    char_cache = {}
-
-    for c in string:
-        if c not in char_cache:
-            char_cache[c] = 0
-        char_cache[c] += 1
-
-    if len(char_cache) * 2 >= len(string):
-        return string
-
     str_builder = []
-    for c in char_cache.items():
-        str_builder.append(c[0])
-        str_builder.append(str(c[1]))
+
+    count = 1
+    current_char = string[1]
+    for c in range(1, len(string)):
+        if string[c] == current_char:
+            count += 1
+        else:
+            str_builder += [current_char, str(count)]
+            count = 1
+            current_char = string[c]
+    str_builder += [current_char, str(count)]
+
+    if len(str_builder) >= len(string):
+        return string
 
     return "".join(str_builder)
 
 
+def decompress_string(string):
+    state = 0
+    current_char = None
+    count = 0
+    str_builder = []
+
+    i = 0
+    while i < len(string):
+        c = string[i]
+        if state == 0:
+            current_char = c
+            state = 1
+            count = 0
+            i += 1
+        else:
+            if str.isnumeric(c):
+                count = count * 10 + int(c)
+                i += 1
+            else:
+                state = 0
+                str_builder += [current_char for _ in range(count)]
+    str_builder += [current_char for _ in range(count)]
+    
+    return "".join(str_builder)
+
+
+# encoder test
 string = "aaabbcccc"
 actual = compress_string(string)
 expected = "a3b2c4"
+assert actual == expected, f"actual: {actual} expected: {expected}"
+
+string = "aaabbaa"
+actual = compress_string(string)
+expected = "a3b2a2"
+assert actual == expected, f"actual: {actual} expected: {expected}"
+
+string = "aaa"
+actual = compress_string(string)
+expected = "a3"
+assert actual == expected, f"actual: {actual} expected: {expected}"
+
+
+# decoder test
+string = "a3b2c4"
+actual = decompress_string(string)
+expected = "aaabbcccc"
 assert actual == expected, f"actual: {actual} expected: {expected}"
